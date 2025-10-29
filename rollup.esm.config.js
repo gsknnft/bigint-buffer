1008010a8c01183ef9b48231a2dc7289b6f3f6ff
gsknnft <gsknnft@gmail.com>
2025-10-29 18:51:28 -0400

    emit dist/index.cjs

--- rollup.esm.config.js
+++ rollup.esm.config.js
@@ -3,10 +3,8 @@ import replace from 'rollup-plugin-replace';
 export default {
     input: 'build/src/index.js',
     output: {
-        file: 'dist/index.cjs',
         format: 'cjs'
     },
-    exeternal: ['bindings'],
     plugins: [
         replace({
             'process.browser': process.env.BROWSER === "true"
