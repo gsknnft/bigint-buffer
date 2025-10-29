9420c6e0b34f098e2eb0b384f4d305d8544fc686
gsknnft <gsknnft@gmail.com>
2025-10-29 18:24:30 -0400

    cjs fix

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ import replace from 'rollup-plugin-replace';
 export default {
     input: 'build/src/index.js',
     output: {
+        file: 'dist/index.cjs',
         format: 'cjs'
     },
+    exeternal: ['bindings'],
     plugins: [
         replace({
             'process.browser': process.env.BROWSER === "true"
